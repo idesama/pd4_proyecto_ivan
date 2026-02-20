2f847833d17d7c77e8e964d65b1a2771328cb8b7
Iván Déniz <ivandenizsantana@alumno.ieselrincon.es>
2026-02-20 21:32:03 +0000

    create user also create list clocks, pending to clear code

--- proyecto/infrastructure/user_repository.py
+++ proyecto/infrastructure/user_repository.py
@@ -1,4 +1,5 @@
 from domain.repository.IUserRepository import IUserRepository
+from domain.entities.user import User
 from infrastructure.db import DB
 
 class UserRepository(IUserRepository):
@@ -6,9 +7,13 @@ class UserRepository(IUserRepository):
     def __init__(self, conection_db=DB()):
         self.conection_db = conection_db
 
-    def add_user(self, user):
+    def add_user(self, user:User):
+        user_clocks = self.conection_db.clocks.get(user.id, None)
+        if user_clocks is None:
+            self.conection_db.clocks[str(user.id)] = []
         self.conection_db.users[user.username] = user
         return True
+    
 
     def get_user_by_username(self, username):
         return self.conection_db.users.get(username, None)
