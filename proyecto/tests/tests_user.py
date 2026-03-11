4c7003f2b40910756e8f3711c352ed6638e81379
Iván Déniz <ivandenizsantana@alumno.ieselrincon.es>
2026-03-11 22:12:40 +0000

    tests user works

--- proyecto/tests/tests_user.py
+++ proyecto/tests/tests_user.py
@@ -3,49 +3,74 @@ from infrastructure.user_repository import UserRepository
 from application.create_user_handler import AddUserHandler
 from application.get_user_handler import GetUserHandler
 from application.commands.create_user_command import CreateUserCommand
+from domain.user_service import UserService
 
 
 class TestsUser(unittest.TestCase):
 
-    def test_add_user(self):
-        handler = AddUserHandler(UserRepository)
-        user = CreateUserCommand(
-            'Abian',
+    def test_add_user_succes(self):
+        user_repository = UserRepository()
+        user_service = UserService(user_repository)
+        handler = AddUserHandler(user_repository, user_service)
+        command = CreateUserCommand(
+            'Pedro',
             '0000',
-            '1'
+            1
         )
-        result = handler.run(user)
+        result = handler.run(command)
         self.assertEqual(result, True)
 
 
+    def test_add_user_error(self):
+        user_repository = UserRepository()
+        user_service = UserService(user_repository)
+        handler = AddUserHandler(user_repository, user_service)
+        command1 = CreateUserCommand(
+            'Abian',
+            '0000',
+            1
+        )
+        command2 = CreateUserCommand(
+            'Abian',
+            '0000',
+            1
+        )
+        result1 = handler.run(command1)
+        result2 = handler.run(command2)
+
+        self.assertTrue(result1)
+        self.assertFalse(result2)
 
 
-    def test_get_user_by_username(self):
-        add_user_handler = AddUserHandler(UserRepository)
-        get_user_handler = GetUserHandler(UserRepository)
+
+    def test_get_user_by_username_succes(self):
+        user_repository = UserRepository()
+        user_service = UserService(user_repository)
+
+        add_user_handler = AddUserHandler(user_repository, user_service)
+        get_user_handler = GetUserHandler(user_repository)
+
         username = 'Ana'
-        user = CreateUserCommand(
+
+        command = CreateUserCommand(
             username,
             '0000',
-            2
+            1
         )
-        result = add_user_handler.run(user)
+        result = add_user_handler.run(command)
         self.assertEqual(result, True)
         user = get_user_handler.run(username)
         self.assertTrue(user)
         self.assertEqual(user.username, username)
 
-    def test_add_user_without_rol(self):
-        add_user_handler = AddUserHandler(UserRepository)
-        get_user_handler = GetUserHandler(UserRepository)
-        username = 'Pedro'
-        user = CreateUserCommand(
-            username,
-            '0000'
+
+    def test_create_user_without_rol(self):
+        user_service = UserService(UserRepository())
+        user = user_service.create_user(
+            'Pepe',
+            '0000'  ,
+            None   
         )
-        result = add_user_handler.run(user)
-        self.assertEqual(result, True)
-        user = get_user_handler.run(username)
         self.assertEqual(user.rol, 2)
 
         
